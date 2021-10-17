81253a814d65289731153459639cd19489210f11
Gopiram Sharma <gsh@hcm-advice.com>
2021-10-17 16:24:30 +0200

    Add project files.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureAuth.Groups.Controllers
+{
+    public class AccountController : Controller
+    {
+        [Authorize]
+        public IActionResult SignOut()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("SignOut", "Account", new { area = "MicrosoftIdentity" });
+        }
+    }
+}
